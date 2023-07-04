2fc93baeaa8512b1955873c479d4e84d3b3ecacd
Nicolas Nieloux <nicolas.nieloux@le-campus-numerique.fr>
2023-07-04 16:42:21 +0200

    first commit

---
+++ src/main/java/com/donjon/ddmicroservice/DDMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.donjon.ddmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DDMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donjon/ddmicroservice/DDMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donjon.ddmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
